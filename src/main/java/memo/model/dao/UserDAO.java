7df4efa45f765952f2488ee5442ba402e151a1a7
alsdnrkim-tb <alsdnrkim@gmail.com>
2025-08-19 13:45:46 +0900

    커밋

--- src/main/java/memo/model/dao/UserDAO.java
+++ src/main/java/memo/model/dao/UserDAO.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 
 public interface UserDAO {
     Long create(String username, String displayName);
-    Optional<UserDTO> findById(Long userId);
-    Optional<UserDTO> findByUsername(Long username);
-    List<UserDTO> findAll(int limit, int offset);
+    // 메서드 시그니처 -> 속성들을 직접 전달
+    // 또다른 DTO로 엮어서...
+    Optional<UserDTO> findById(Long userId); // PK
+    Optional<UserDTO> findByUsername(String username); // 생성할 때 쓴 이름 -> 로그인
+    List<UserDTO> findAll(int limit, int offset); // 50개 있다고 하면 5개씩 불러오게 ...
 }
